809ddc5c40c0f86a160c8f5ec69460dbe52d0d11
Kacper Szatkowski <misko2597@gmail.com>
2022-06-03 17:59:45 +0100

    s

---
+++ js/game.js
@@ -0,0 +1,17 @@
+var config = {
+	type:Phaser.AUTO,
+	//width:1120,
+	//height:640,
+	scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        width: 1120,
+        height: 640
+    },
+	physics: {
+    default: 'arcade'
+	},
+	scene: [Home]
+};
+
+var game = new Phaser.Game(config);
